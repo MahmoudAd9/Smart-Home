ca57c6e9e6a02192464abd230682c6921652479f
Youssef Chakir <youssef.chakir@e-polytechnique.ma>
2024-01-12 13:26:10 +0100

    Last change

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+//import 'package:smart_home_flutter_ui/LoginScreen.dart';
 import 'package:smart_home_flutter_ui/constants/app_colors.dart';
-import 'package:smart_home_flutter_ui/screen/smart_home_screen.dart';
+import 'package:smart_home_flutter_ui/firebase_options.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  print("good here");
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  print("good here also");
   runApp(const MyApp());
 }
 
@@ -13,13 +22,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Smart House',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: AppColor.bgColor,
-        fontFamily: "Poppins"
-      ),
-      home: const SmartHomeScreen(),
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: AppColor.bgColor,
+          fontFamily: "Poppins"),
+      home: Center(),
     );
   }
+
+ /* void setData() async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    DocumentReference documentReference =
+        firestore.collection('users').doc("lNDFYyG97pxZqot4TnI4");
+
+    try {
+      await documentReference.set({
+        'field1': 'value1',
+        'field2': 'value2',
+        // Add other fields as needed
+      });
+      print('Document successfully set.');
+    } catch (e) {
+      print('Error setting document: $e');
+    }
+  }*/
 }
